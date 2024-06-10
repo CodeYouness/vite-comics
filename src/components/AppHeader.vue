c53776ef30dca290f48c4eb74ff90458119d4260
CodeYouness <ylijassi@gmail.com>
2024-06-10 17:22:25 +0200

    added array with header link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,6 +2,18 @@
 export default {
     data() {
         return {
+            headerLink: [
+    { id: 1, title: "characters", active: false },
+    { id: 2, title: "comics", active: true },
+    { id: 3, title: "movies", active: false },
+    { id: 4, title: "tv", active: false },
+    { id: 5, title: "games", active: false },
+    { id: 6, title: "collectibles", active: false },
+    { id: 7, title: "videos", active: false },
+    { id: 8, title: "fan", active: false },
+    { id: 9, title: "news", active: false },
+    { id: 10, title: "shop", active: false }
+]
         }
     },
 }
@@ -9,7 +21,7 @@ export default {
 
 <template>
     <div>
-        <p>header</p>
+        <p v-for="title in headerLink">{{title.title}}</p>
     </div>
 </template>
 
