da1b688a029603b4917bd07b6af00b9640e3ca57
CodeYouness <ylijassi@gmail.com>
2024-06-11 00:07:45 +0200

    added array for jumbotron

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data() {
+        return {
+            merch: [
+                { id:1, product: 'digital comics', url: 'src/assets/img/buy-comics-digital-comics.png'},
+                { id:2, product: 'dc merchandise', url: 'src/assets/img/buy-comics-merchandise.png'},
+                { id:3, product: 'subscription', url: 'src/assets/img/buy-comics-subscriptions.png'},
+                { id:4, product: 'comic shop locator', url: 'src/assets/img/buy-comics-shop-locator.png'},
+                { id:5, product: 'dc power visa', url: 'src/assets/img/buy-dc-power-visa.svg'}
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
